2af1916138455087e6da95265c4393e428981fcd
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-09-06 13:33:59 -0400

    PlayerDashing
    Player will dash in the direction the player is moving.  You can change the speed of the dash and rate in the editor.  Will later accommodate dash in the direction of the mouse.

--- Bullet-Havoc/Assets/PlayerMovement.cs
+++ Bullet-Havoc/Assets/PlayerMovement.cs
@@ -6,30 +6,60 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     //player Movement
-    [SerializeField] private float _speed;
+    [Header("Player's Overal Speed")]
+    [SerializeField] private float playerMoveSpeed = 6;
+    [SerializeField] private float activePlayerMoveSpeed;
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
 
     //Player Dash
     [Header("Player Dash")]
-    [SerializeField] private int dashSpeed;
-    [SerializeField] private float dashCounter;
-    [SerializeField] private float dashCooldown;
-    
+    [SerializeField] private int dashSpeed = 10;
+    public float dashRate = 2f;
+    float nextDashTime = 0f;
+    bool isDashing = false;
+
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
 
     }
-
+    private void Start()
+    {
+        activePlayerMoveSpeed = playerMoveSpeed;
+    }
     private void FixedUpdate()
     {
-        _rigidbody.velocity = _movementInput * _speed;
+        _rigidbody.velocity = _movementInput * activePlayerMoveSpeed;
+
+        if (Time.time >= nextDashTime) 
+        {
+            nextDashTime = Time.time + 1f / dashRate;
+
+            activePlayerMoveSpeed = playerMoveSpeed;
+
+            isDashing = false;
+        } 
     }
 
     private void OnMove(InputValue inputValue)
     {
         _movementInput = inputValue.Get<Vector2>();
     }
+
+    private void OnDash(InputValue inputValue)
+    {
+        if (!isDashing)
+        {
+            Debug.Log("Player is dashing");
+            activePlayerMoveSpeed = dashSpeed;
+
+            isDashing = true;
+
+        }
+
+
+    }
+
 }
