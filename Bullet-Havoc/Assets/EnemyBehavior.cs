6358da8d2adc2b1e309e6db01d328965274ae6f3
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-09-12 13:22:18 -0400

    Adds the Worm
    Adds the Worm

--- Bullet-Havoc/Assets/EnemyBehavior.cs
+++ Bullet-Havoc/Assets/EnemyBehavior.cs
@@ -1,27 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class EnemyBehavior : MonoBehaviour
 {
     public GameObject player;
     public float activeEnemySpeed;
     public float enemySpeed;
+    public float enemyNumber;
 
     private float distance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemySpeed = enemySpeed*Mathf.Pow(0.97f,enemyNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
+        distance = Vector2.Distance(transform.position, Input.mousePosition.normalized);
+        Vector2 direction = Input.mousePosition.normalized - transform.position;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(this.transform.position, Input.mousePosition.normalized, enemySpeed * Time.deltaTime);
     }
 }
