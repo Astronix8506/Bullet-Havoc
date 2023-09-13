d0210fb1e707e90140dd764d38fb093ecc0a8dda
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-09-13 14:18:27 -0400

    Enemy Ai Push 1
    Adds a bit more reactive AI

--- Bullet-Havoc/Assets/EnemyBehavior.cs
+++ Bullet-Havoc/Assets/EnemyBehavior.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     public GameObject player;
+    public GameObject frontNode;
+    public GameObject bottomNode;
+    public GameObject backNode;
+    public GameObject topNode;
     public float activeEnemySpeed;
     public float enemySpeed;
 
     private float distance;
 
+    public LineRenderer lr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeEnemySpeed = enemySpeed;
     }
 
     // Update is called once per frame
@@ -22,10 +27,32 @@ public class EnemyBehavior : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
-        if (distance > 1)
+        if (distance > 4)
         {
-
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+            activeEnemySpeed = enemySpeed;
+            //Allows enemy to follow player
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, activeEnemySpeed * Time.deltaTime);
         }
+        else
+        {
+            activeEnemySpeed = 5;
+            transform.position = Vector2.MoveTowards(this.transform.position, backNode.transform.position, activeEnemySpeed * Time.deltaTime);
+        }
+
+
+        /*
+        void DrawCircle(int steps, float radius)
+        {
+            lr.positionCount = steps;
+
+            for(int currentStep = 0; currentStep < steps; ++currentStep)
+            {
+                float circumferenceProgress = (float)currentStep / steps;
+
+                float currentRadian = circumferenceProgress * 2 * Mathf.PI ;
+
+
+            }
+        }*/
     }
 }
