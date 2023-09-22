4aa4bf81d87a0ffef5c4f1687f092950483567f1
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-09-22 14:19:24 -0400

    More Behavior tree stuff
    adds 3 scripts

--- Bullet-Havoc/Assets/Node.cs
+++ Bullet-Havoc/Assets/Node.cs
@@ -5,7 +5,7 @@ namespace BehaviorTree
 {
     public enum NodeState
     {
-        running,
+        RUNNING,
         SUCCESS,
         FAILURE
 
@@ -33,12 +33,48 @@ namespace BehaviorTree
             children.Add(node);
         }
 
-        public virtual NodeState Evaluate() => NodeState.FAILURE
+        public virtual NodeState Evaluate() => NodeState.FAILURE;
 
-            public void SetData(string key, object value)
+        public void SetData(string key, object value)
         {
             _dataContext[key] = value;
         }
-    }
 
+        public object GetData(string key)
+        {
+            object value = null;
+            if (_dataContext.TryGetValue(key, out value))
+                return value;
+
+            Node node = null;
+            while (node != null) 
+            { 
+                value = node.GetData(key);
+                if (value != null) 
+                    return value;
+                node = node.parent;
+            }
+            return null;
+        }
+
+        public bool ClearData(string key)
+        {
+ 
+            if (_dataContext.ContainsKey(key))
+            {
+                _dataContext.Remove(key);
+                return true;
+            }
+
+            Node node = parent;
+            while (node != null)
+            {
+                bool cleared = node.ClearData(key);
+                if (cleared)
+                    return true;
+                node = node.parent;
+            }
+            return false;
+        }
+    }
 }
