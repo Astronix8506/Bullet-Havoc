cbd2c0fcb58029711be66eb4a85e94a9389d0253
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2023-09-28 11:49:12 -0400

    Behavior tree

--- Bullet-Havoc/Assets/Selector.cs
+++ Bullet-Havoc/Assets/Selector.cs
@@ -1,40 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace BehaviorTree
 {
-    public class Sequence : Node
+    public class Selector : Node
     {
-        public Sequence() : base() { }
-        public Sequence(List<Node> children) : base(children) { }
+        public Selector() : base() { }
+        public Selector(List<Node> children) : base(children) { }
         public override NodeState Evaluate()
         {
-            bool anyChildIsRunning = false;
-
             foreach (Node node in children)
             {
                 switch (node.Evaluate())
                 {
                     case NodeState.FAILURE:
-                        state = NodeState.FAILURE;
-                        return state;
+                        continue;
                     case NodeState.SUCCESS:
+                        state = NodeState.SUCCESS;
                         continue;
                     case NodeState.RUNNING:
-                        anyChildIsRunning = true;
+                        state = NodeState.RUNNING;
                         continue;
                     default:
-                        state = NodeState.SUCCESS;
                         return state;
                 }
             }
-            state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+            state = NodeState.FAILURE;
             return state;
 
         }
     }
 
 }
-
-
