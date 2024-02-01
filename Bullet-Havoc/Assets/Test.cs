92f310873120b0deecbae42868fe2ac7e56ee7c4
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2024-02-01 14:13:57 -0500

    Enemy Ai
    Made a script which allows the enemy to start tracking and following the player using A*

---
+++ Bullet-Havoc/Assets/Test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
+    void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+            Debug.DrawLine(ray.origin, hit.point);
+    }
+}
